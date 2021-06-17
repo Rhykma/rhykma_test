3b6fd1bfed31024376017fd44f5f20ee1a7da812
minedgeekble <86029271+minedgeekble@users.noreply.github.com>
2021-06-17 09:44:50 +0900

    main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+def on_button_pressed_a():
+    global a
+    if a == 0:
+        a = 1
+    elif a == 1:
+        a = 2
+    elif a == 2:
+        a = 3
+    elif a == 3:
+        a = 4
+    elif a == 4:
+        a = 5
+    elif a == 5:
+        a = 1
+    else:
+        pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+a = 0
+a = 1
+
 def on_forever():
-    pass
+    if a == 1:
+        basic.show_icon(IconNames.HEART)
+    if a == 2:
+        basic.show_icon(IconNames.SMALL_HEART)
+    if a == 3:
+        basic.show_icon(IconNames.YES)
+    if a == 4:
+        basic.show_icon(IconNames.NO)
+    if a == 5:
+        basic.show_icon(IconNames.HAPPY)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    if (a == 0) {
+        a = 1
+    } else if (a == 1) {
+        a = 2
+    } else if (a == 2) {
+        a = 3
+    } else if (a == 3) {
+        a = 4
+    } else if (a == 4) {
+        a = 5
+    } else if (a == 5) {
+        a = 1
+    } else {
+    	
+    }
+})
+let a = 0
+a = 1
 basic.forever(function () {
-	
+    if (a == 1) {
+        basic.showIcon(IconNames.Heart)
+    }
+    if (a == 2) {
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    if (a == 3) {
+        basic.showIcon(IconNames.Yes)
+    }
+    if (a == 4) {
+        basic.showIcon(IconNames.No)
+    }
+    if (a == 5) {
+        basic.showIcon(IconNames.Happy)
+    }
 })
